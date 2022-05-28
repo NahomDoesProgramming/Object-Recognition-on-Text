c200efb865dd8c1def44fd0308ace7f51025ced7
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2022-05-27 18:14:26 -0700

    Add files via upload
    E

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status = "";
-text = "";
+txet = "";
+objects = [];
 function setup()
 {
     canvas = createCanvas(480, 380);
@@ -12,7 +13,7 @@ function start()
 {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
-    text = document.getElementById("textarea").value;
+    txet = document.getElementById("textarea").value;
 }
 function modelLoaded()
 {
@@ -22,4 +23,35 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+    if(status != "" && txet == objects)
+    {
+
+      for (i = 0; i < objects.length; i++)
+      {     
+        videoLiveView.stop();     
+        document.getElementById("instruction").innerHTML = "Object mentioned found!!";
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].labe l + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#2be");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        objectDetector.detect(gotResult);
+        speech = window.speechSynthesis;
+        var utterthis = new SpeechSynthesisUtterance("Object mentioned found!!");
+        speak(utterthis);
+      }
+    }
+    else
+    {
+        document.getElementById("instruction").innerHTML = "Could not find object. :(";
+    }
+}
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
 }
